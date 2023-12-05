c78f39b3915e0874b96c943af15589669d0ab77d
Walter Bernal <wltrbm@gmail.com>
2023-12-05 16:35:12 -0700

    Update of DocumentHashStorage smart contract and compilation

--- contracts/DocumentHashStorage.sol
+++ contracts/DocumentHashStorage.sol
@@ -2,9 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract DocumentHashStorage {
+  address private owner;
   mapping(string => string) private documentHashes;
 
-  function storeDocumentHash(string memory uId, string memory documentHash) external {
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only the owner can call this function");
+    _;
+  }
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function storeDocumentHash(string memory uId, string memory documentHash) external onlyOwner {
     require(bytes(uId).length > 0, "Transaction code must not be empty");
     require(bytes(documentHash).length > 0, "Document hash must not be empty");
     documentHashes[uId] = documentHash;
