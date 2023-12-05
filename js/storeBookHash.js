176cad99ce19002a12dfca9f10a8ad45f09bf97a
Walter Bernal <wltrbm@gmail.com>
2023-12-05 15:06:04 -0700

    udpate to use document keyword instead of book

--- js/storeBookHash.js
+++ js/storeBookHash.js
@@ -4,26 +4,26 @@ const { privateKey, nodeUrl, contractAddress } = require("./constants")
 const config = require('dotenv').config
 config()
 
-async function storeBookHash(transactionCode, bookHash) {
+async function storeDocumentHash(transactionCode, documentHash) {
   const provider = ethers.getDefaultProvider(nodeUrl)
   const wallet = new ethers.Wallet(privateKey, provider)
   const contractABI = getABI("contracts/DocumentHashStorage.sol")
 
   const contract = new ethers.Contract(contractAddress, contractABI, wallet)
 
-  const transaction = await contract.storeBookHash(transactionCode, bookHash)
+  const transaction = await contract.storeDocumentHash(transactionCode, documentHash)
   const contractTransacitonReceipt = await transaction.wait()
   console.log(contractTransacitonReceipt)
-  console.log("Book hash stored successfully.")
+  console.log("Document hash stored successfully.")
 }
 
-const bookHash1 = "0x123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdex"
-const bookHash2 = "0x123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdey"
-const bookHash3 = "0x123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdez"
+const documentHash1 = "0x123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdex"
+const documentHash2 = "0x123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdey"
+const documentHash3 = "0x123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdez"
 const transactionCode1 = "0-1278-5049-X"
 const transactionCode2 = "0-1278-5049-Y"
 const transactionCode3 = "0-1278-5049-Z"
 
-storeBookHash(transactionCode1, bookHash1)
+storeDocumentHash(transactionCode3, documentHash3)
 
-module.exports = storeBookHash
+module.exports = storeDocumentHash
